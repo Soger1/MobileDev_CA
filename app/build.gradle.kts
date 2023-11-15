c49e3f4f6ab30285e14bb07ac764122b268a9c50
Ethan <ethanholohan1@gmail.com>
2023-11-15 13:17:08 +0000

    Basic Timer Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.md_project"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.md_project"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/md_project/MainActivity.java
+++ app/src/main/java/com/example/md_project/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.md_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView gameTimer;
+    private Button startTimer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gameTimer = findViewById(R.id.TV_Timer);
+        startTimer = findViewById(R.id.BTN_StartTimer);
+
+        startTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CountDownTimer(50000, 1000) {
+                    public void onTick(long millisUntilFinished) {
+                        NumberFormat TimerFormat = new DecimalFormat("00");
+                        long hour = (millisUntilFinished / 3600000) % 24;
+                        long min = (millisUntilFinished / 60000) % 60;
+                        long sec = (millisUntilFinished / 1000) % 60;
+                        gameTimer.setText(TimerFormat.format(hour) + ":" + TimerFormat.format(min) + ":" + TimerFormat.format(sec));
+                    }
+
+                    public void onFinish() {
+                        gameTimer.setText("00:00:00");
+                    }
+                }.start();
+            }
+        });
+        }
+
+
     }
-}
